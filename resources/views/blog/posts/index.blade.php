c451fa6a98289467c6cc34364587de5c554e2cee
Boris <bobabonanadz@mail.ru>
2020-01-03 18:10:03 +0300

    Auth

--- resources/views/blog/posts/index.blade.php
+++ resources/views/blog/posts/index.blade.php
@@ -1,7 +1,13 @@
-<table border="1">
+@extends('layouts.app')
+
+@section('content')
+<table class="table table-bordered">
     @foreach ($item as $i)
     <tr>
-        <td> {{ $i->id }}</td>
+        <td>{{ $i->id }}</td>
+        <td>{{ $i->title }}</td>
+        <td>{{ $i->content_raw }}</td>
     </tr>
     @endforeach
 </table>
+@endsection
